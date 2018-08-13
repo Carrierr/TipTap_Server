65e9d376932e730f802af7adb6b5ec239ad26311
giseoplee <llgs901@naver.com>
2018-08-13 11:44:38 +0900

    (2018-08-13) update by giseop.lee : update env setting, docker script

--- config/index.js
+++ config/index.js
@@ -5,6 +5,7 @@
 const path = require('path');
 const development = require('./env/development');
 const production = require('./env/production');
+const ec2 = require('./env/ec2');
 
 const defaults = {
     root: path.join(__dirname, '..')
@@ -13,4 +14,5 @@ const defaults = {
 module.exports = {
     development: Object.assign({}, development, defaults),
     production: Object.assign({}, production, defaults),
+    ec2: Object.assign({}, ec2, defaults),
 }[process.env.NODE_ENV || 'development']
