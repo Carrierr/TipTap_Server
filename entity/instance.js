6739206ae0617d2c4645297dd9e5da15ab6cef7c
giseoplee <llgs901@naver.com>
2018-09-22 16:18:08 +0900

    (2018-09-22) update by giseop.lee : update sequelize timezone (Asia/Seoul)

--- entity/instance.js
+++ entity/instance.js
@@ -13,7 +13,7 @@ const sequelize = new Sequelize(
       idle: config.store.ConnectionIdle,
       waitForConnections: false /* 사용 가능한 커넥션이 없을 경우 바로 ERROR를 return | true일 경우 대기 */
     },
-    // timezone: "+09:00",
+    timezone: 'Asia/Seoul',
     logging: true
   });
 
